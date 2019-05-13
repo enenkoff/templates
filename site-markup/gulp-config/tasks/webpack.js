506e6e8771a0316ff77f0a9487e49c3afb504de8
enenkoff <enenkoff@mail.ru>
2019-05-13 21:27:37 +0300

    webpack jquery ext

--- site-markup/gulp-config/tasks/webpack.js
+++ site-markup/gulp-config/tasks/webpack.js
@@ -3,7 +3,16 @@ module.exports = function (gulp, plugins, path_src, path_dest) {
     let webpack = require('webpack'),
         webpackStream = require('webpack-stream');
 
+    let onError = function(err) {
+        plugins.notify.onError({
+            title: 'Error in ' + err.plugin,
+            message: err.message,
+        })(err);
+        this.emit('end');
+    };
+
     return gulp.src(path_src + 'app.js')
+            .pipe(plugins.plumber({ errorHandler: onError }))
             .pipe(webpackStream({
                 output: {
                     filename: 'app.js',
@@ -20,9 +29,13 @@ module.exports = function (gulp, plugins, path_src, path_dest) {
                         }
                     ]
                 },
-                externals: {
-                    jquery: 'jQuery'
-                }
+                plugins: [
+                    new webpack.ProvidePlugin({
+                        $: 'jquery/dist/jquery.min.js',
+                        jQuery: 'jquery/dist/jquery.min.js',
+                        'window.jQuery': 'jquery/dist/jquery.min.js',
+                    }),
+                ],
             }))
             .pipe(plugins.uglify())
             .pipe(plugins.rename({ suffix: '.min' }))
